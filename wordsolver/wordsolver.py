a83680ad0bc739b840d7edd2298397b8eeaf195d
Nasreen123 <Nasreen123@users.noreply.github.com>
2017-06-08 10:40:05 +0100

    refactor wordsolver.py

--- wordsolver/wordsolver.py
+++ wordsolver/wordsolver.py
@@ -1,17 +1,10 @@
-
 import requests
-#import pprint
-#import urllib.request
-#from bs4 import BeautifulSoup
 import re
 
-"""target = '????s??'
-clue_words = ['accumulate', 'accumulates']
-length = len(target)"""
 
+# GET POTENTIAL MATCHES
 
 def get_matches(target):
-    ########### FIND POTENTIAL MATCHES ###########
     length = len(target)
     matches = []
 
@@ -35,111 +28,97 @@ def get_matches(target):
     word_list.close()
     return matches
 
-    """########### GET SUMMARY FOR EACH POTENTIAL MATCH ###########
-
-    wiki_texts = [[] for x in range(len(matches))]
-    #pp = pprint.PrettyPrinter(indent=3)
-    #print('\n\nLooking for text ')
-    for i, word in enumerate(matches):
-        entries = []
-        query = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&redirects=1&format=json&titles=' + word
-        r = requests.get(query)
-        result_object = r.json()
-
-        key = list(result_object['query']['pages'].keys())[0]
-        if key == '-1': # no result - look up in dictionary
-            query = 'https://en.wiktionary.org/wiki/' + word
-            result = requests.get(query) #urllib.request.urlopen(query).read()
-            text = result.text
-            try:
-                start = re.search('id="Translations', text) #(.+?)style="text-align:left;">
-                text = text[start.end():]
-                start = re.search('style="text-align:left;">', text)
-                text = text[start.end():]
-                end = re.search("</div>", text)
-                text = text[:end.start()]
-                entries.append(text)
-            except:
-                entries.append('not found')
-                wiki_texts[i].append(entries)
-
-        else:
-            extract = result_object['query']['pages'][key]['extract']
-
-            if 'may refer to' in extract: # result is a disambiguation page
-
-                try:
-                    query = 'https://en.wikipedia.org/wiki/' + word
-                    result = requests.get(query) #urllib.request.urlopen(query).read()
-                    text = result.text
-
-                    start = re.search(' may refer to:</p>', text)
-                    end = re.search('<span class="mw-headline" id="See_also">', text)
-
-                    text = text[start.end():end.start()]
-                    text = re.sub('<.+?>', '', text)
-                    text = re.sub('\n\n\n', '\n', text)
-                    text = re.sub('\n\n', '\n', text)
-
-                    entries = text.split('\n')
-
-                except:
-                    entries.append('not found')
-                    wiki_texts[i].append(entries)
 
 
-            else:
-                entries.append(extract) #if it isn't a disambiguation page we want it
+# GET WORD MEANING
+
+def get_meanings(matches):
+    matches_and_meanings = {}
+
+    for word in matches:
+        meaning = get_wikipedia_extract(word)
+        if meaning == None:
+            meaning = '/' + get_wikipedia_disambiguation_page(word)
+        if meaning == None:
+            meaning = get_wictionary_meaning(word)
+
+        meaning = clean(meaning)
+        matches_and_meanings[word] = meaning
+
+    return matches_and_meanings
 
 
-            wiki_texts[i].append(entries)#entries is a list with the final info for this word
-            #print('appended for ', word)
+def clean(text):
+    #remove html tags
+    text = re.sub('<.+?>', '', text)
+    #remove double line breaks
+    text = re.sub('\n\n\n', '\n', text)
+    text = re.sub('\n\n', '\n', text)
+    return text
 
 
-    ########### FIND THE BEST MATCHES BY SUMMARY CONTENT ###########
-
-    scores = [0 for x in matches]
-
-    for i in range(len(matches)):
-        for word in clue_words:
-            for l in wiki_texts[i]:
-                for w in l:
-                    if word in w:
-                            scores[i] = scores[i] + 1
-
-
-    ########### ORDER THE RESULTS ###########
-
-    results = []
+def get_wikipedia_extract(word):
+    query = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&redirects=1&format=json&titles=' + word
+    r = requests.get(query)
+    result_object = r.json()
+    key = list(result_object['query']['pages'].keys())[0]
+    if key != None:
+        extract = result_object['query']['pages'][key]['extract']
+        if '</b> may refer to:</p>' not in extract:
+            return extract
+    return None
 
 
-    maxi = 1
+def get_wikipedia_disambiguation_page(word):
+    query = 'https://en.wikipedia.org/wiki/' + word
+    #try here?
+    result = requests.get(query)
+    text = result.text
 
-    while len(scores) > 0 and maxi > 0:
-        maxi = max(scores)
-        if maxi > 0:
-            ind = scores.index(maxi)
-            result = [matches[ind], scores[i], wiki_texts[ind]]
-            results.append(result)
-            matches.pop(ind)
-            scores.pop(ind)
-            wiki_texts.pop(ind)"""
+    start = re.search(' may refer to:</p>', text)
+    end = re.search('<span class="mw-headline" id="See_also">', text)
 
-    """
-    print('MATCHES FOR : "', target, '" (Clue words: ', clue_words, ' )\n')
+    text = text[start.end():end.start()]
 
-    print('The most likely match: \n\n')
+    return text
 
 
-    for result in results:
-        print(result[0])
-        print(result[2], '\n')
+def get_wictionary_meaning(word):
+    query = 'https://en.wiktionary.org/wiki/' + word
+    result = requests.get(query) #urllib.request.urlopen(query).read()
+    text = result.text
+    try:
+        start = re.search('id="Translations', text) #(.+?)style="text-align:left;">
+        text = text[start.end():]
+        start = re.search('style="text-align:left;">', text)
+        text = text[start.end():]
+        end = re.search("</div>", text)
+        text = text[:end.start()]
+        return meaning
+    except:
+        return None
 
 
 
-    print('\nnOther potential matches are: \n')
+# RATE MEANINGS BASED ON CLUES TO FIND THE BEST MATCHES
 
-    for i in range(len(matches)):
-        print(matches[i])
-        for entry in wiki_texts[i]:
-            print(entry, '\n')"""
+def get_score(meaning, clues):
+    score = 0
+    for clue_word in clues:
+        if clue_word in meaning:
+            score = score + 1
+    return score
+
+
+def get_good_matches(matches_meanings_dict, clues):
+    scores_dict = {} # might use this later to give better suggestions
+    good_matches = []
+
+    for word, meaning in matches_meanings_dict.items():
+        score = get_score(meaning, clues)
+        scores_dict[word] = score
+        if score > 0:
+            good_matches.append(word)
+        print(word, score)
+
+    return good_matches

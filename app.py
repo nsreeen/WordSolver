64ff61031af99859cdd3460f1051a6d5c663937c
Nasreen123 <Nasreen123@users.noreply.github.com>
2017-06-10 13:07:27 +0100

    Fix port: app.py takes the port as a command line argument, update this is Procfile

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-import os
+import sys
 from flask import Flask, render_template, session, request
 from wordsolver.wordsolver import get_matches, get_meanings, get_good_matches
 
 app = Flask(__name__)
 
 app.secret_key = 'test'
-
-port = int(os.environ.get('PORT', 5000))
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -16,6 +13,7 @@ def index():
         clue = request.form['clue']
 
         matches = get_matches(target)
+        print(matches)
         matches_and_meanings = get_meanings(matches)
         ordered_matches = get_good_matches(matches_and_meanings, clue)
         return render_template('index.html', target=target, clue='', matches_and_meanings=matches_and_meanings, ordered_matches=ordered_matches) #session['target'], clue=session['clue'])
@@ -25,5 +23,5 @@ def index():
 
 
 if __name__ == '__main__':
+    port = int(sys.argv[1])
     app.run(host='0.0.0.0', port=port)
-    #app.run()

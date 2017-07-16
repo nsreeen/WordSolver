d3941688cd1923141f72d26db5885c163fb0f7fc
Nasreen123 <Nasreen123@users.noreply.github.com>
2017-07-16 18:46:54 +0100

    Add some styling

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 import sys
 from flask import render_template, session, request, jsonify
-from wordsolver.wordsolver import get_matches, get_meanings, get_good_matches, get_meaning
+from wordsolver.wordsolver import get_matches, get_meaning
 from app.forms import WordForm
 from app import app
 
@@ -8,13 +8,10 @@ from app import app
 def index():
     form = WordForm()
 
-    if request.method == 'POST': 
-        print(request) 
+    if request.method == 'POST':
         target = form.target.data
         form.target.data = ""
-
         matches = get_matches(target)
-        print(matches)
         return render_template('results.html', target=target, form=form, matches=matches)
     else:
         return render_template('index.html', target='', form=form, matches='')
@@ -28,6 +25,3 @@ def getmeaning():
     word = request.form['word']
     meaning = get_meaning(word)
     return jsonify(meaning=meaning)
-
-
-
